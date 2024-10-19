191c6ac4e5e7ab3047e5deed13142fc37d9a9b01
Oleksandr Mykhailyk <stekloduv45@gmail.com>
2024-10-19 10:45:37 +0200

    add it-company-task-manager-s5cf.onrender.com in ALLOWED_HOSTS

--- IT_Company_Task_Manager/settings.py
+++ IT_Company_Task_Manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "it-company-task-manager-s5cf.onrender.com"]
 
 
 # Application definition
