594da7db1ac059661d18227486987b842d68beda
Oleksandr Mykhailyk <stekloduv45@gmail.com>
2024-10-21 13:08:25 +0200

    reduce the size of the .gitignore file to contain only entities related to the project
    Register models in the admin.py
    
    Revise and remove static files that are not needed from the repository

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TaskType, Position, Worker, Task
+
+admin.site.register(TaskType)
+admin.site.register(Position)
+admin.site.register(Worker)
+admin.site.register(Task)
